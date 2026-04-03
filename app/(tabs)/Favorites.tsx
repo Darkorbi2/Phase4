222c0a12c7b2c5f41b5eabe37d1d2475bceda9db
Nathanael Lee <nathanael.lee@edu.sait.ca>
2026-04-02 19:02:24 -0600

    play song, pause song, better organization

--- app/(tabs)/Favorites.tsx
+++ app/(tabs)/Favorites.tsx
@@ -1,4 +1,8 @@
+import { Song } from "@/components/useSongs";
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
+
+const [favoriteSongs, setSongs] = useState<Song[]>([]);
 
 export default function Favorites() {
   return <View></View>;
